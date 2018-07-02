5faffac70e5aa449bb0a8c413ae843c9590fef3b
Noah Israel <noahmvf@gmail.com>
2018-07-02 14:19:56 -0700

    linter erros fixed

--- src/__test__/binary-tree.test.js
+++ src/__test__/binary-tree.test.js
@@ -1,12 +1,10 @@
 'use strict';
 
 import binaryTree from '../model/binary-tree';
-import { preOrderTraversal } from '../lib/traversals';
-import { postOrderTraversal } from '../lib/traversals';
-import { inOrderTraversal } from '../lib/traversals';
+import { preOrderTraversal, postOrderTraversal, inOrderTraversal } from '../lib/traversals';
 
-describe('PRE-ORDER TRAVERSAL' , () => {
-  test('Expecting a string of visited nodes as 1, 2, 6, 7, 8, 9, 3, 4, 5, ' , () => {
+describe('PRE-ORDER TRAVERSAL', () => {
+  test('Expecting a string of visited nodes as 1, 2, 6, 7, 8, 9, 3, 4, 5, ', () => {
     let str = '';
     preOrderTraversal(binaryTree.root, (nodeValue) => {
       str += `${nodeValue}, `;
@@ -15,8 +13,8 @@ describe('PRE-ORDER TRAVERSAL' , () => {
   });
 });
 
-describe('POST-ORDER TRAVERSAL' , () => {
-  test('Expecting a string of visited nodes as 8, 9, 7, 6, 2, 4, 5, 3, 1, ' , () => {
+describe('POST-ORDER TRAVERSAL', () => {
+  test('Expecting a string of visited nodes as 8, 9, 7, 6, 2, 4, 5, 3, 1, ', () => {
     let str = '';
     postOrderTraversal(binaryTree.root, (nodeValue) => {
       str += `${nodeValue}, `;
@@ -25,8 +23,8 @@ describe('POST-ORDER TRAVERSAL' , () => {
   });
 });
 
-describe('IN-ORDER TRAVERSAL' , () => {
-  test('Expecting a string of visited nodes as 6, 8, 7, 9, 2, 1, 4, 3, 5, ' , () => {
+describe('IN-ORDER TRAVERSAL', () => {
+  test('Expecting a string of visited nodes as 6, 8, 7, 9, 2, 1, 4, 3, 5, ', () => {
     let str = '';
     inOrderTraversal(binaryTree.root, (nodeValue) => {
       str += `${nodeValue}, `;
